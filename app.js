f07fe5a8338f5325d7545e7add01d48dcb6c5881
Rufan Mirganiyev <rm14751n@pace.edu>
2023-01-13 21:06:26 -0500

    env changed

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.msz3ahp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://refresher:refresher@cluster0.msz3ahp.mongodb.net/mern?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
